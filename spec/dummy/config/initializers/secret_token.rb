60cec6d31980c631282844dfc61c1c56e5d89b10
Dan Andrews <daniel.raymond.andrews@gmail.com>
2017-01-04 10:33:01 -0600

    update dummy app to rails 4

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '7fae989fc55949807eede0078ff2a5462e64c74e01e6c72a2900cb5d26f68cac114c8a854dda93e1b7675bf5a0d93ba105bf245176fa28a318c1cb5778a8ec0f'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '4d51a720f7cfb3262184082c149aea72ee95a7e9fb7dd5ef3766f1caa602e5b306693129cd452ab8fd35753fad0e95489837277c189c5b69daacbaf28ccd3507'
